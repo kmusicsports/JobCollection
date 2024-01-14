8db9511838a088c1db61aef1aa5da55b4a46a776
kmusicsports <naokik.musicsports1027@gmail.com>
2024-01-14 16:38:10 +0900

    add CompanyController class to company_controller.py

--- python/app/controller/company_controller.py
+++ python/app/controller/company_controller.py
@@ -3,11 +3,13 @@ from flask import Blueprint, render_template
 company_page = Blueprint("company_page", __name__, url_prefix="/company")
 
 
-@company_page.route("/list")
-def show_list():
-    return render_template(
-        "companyList.html",
-        context={
-            "message": "Hello, 就集!"
-        },
-    )
+class CompanyController:
+
+    @company_page.route("/list")
+    def show_list():
+        return render_template(
+            "companyList.html",
+            context={
+                "message": "Hello, 就集!"
+            },
+        )
